bd379f5d9071d9c56ccd0f086a12bffa89047e04
Floran MARTEL <floran.martel@guillemot.fr>
2024-06-05 14:57:52 +0200

    correction

--- model/convertiseur.py
+++ model/convertiseur.py
@@ -2,57 +2,42 @@ import csv
 
 def csvToWpjson(dir):
 
-    result = "a:"
+    resultList = []
 
-    with open(dir, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-
-        compteurLigne = 0
-
-
-        stringlistVal = "{"
-        for rowindex,row in enumerate(spamreader):
-            if rowindex != 0:
-                                 
-                if rowindex != 1:
-                        stringlistVal += ";"
-                
-                stringlistVal += "i:"+str(rowindex-1)+";a:"+str(len(row))+":"
-
-
-                stringValeur = "{"
-                for elementIndex,element in enumerate(row):
-                    if elementIndex != 0:
-                        stringValeur += ";"
-                    
-                    stringValeur += "i:"+str(elementIndex)+";s:"+str(len(element))+":'"+element+"'"
-                stringValeur += "}"
-
-                stringlistVal += stringValeur
-
-                compteurLigne += 1
-            
-
-        stringlistVal += "}"
+    with open(dir, newline='',encoding="utf-8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=";")
+        numcol = len(next(spamreader))
+        resultList = [""]*(numcol-1)
         
+        compteurligne = 0
+        for rowindex,row in enumerate(spamreader):
+            compteurligne += 1
 
-        result += str(compteurLigne)+":"+stringlistVal
+            strlistTranslation = ""
+
+            #i:0;a:2:{i:0;s:2:\"je\";i:1;s:
+            strlistTranslation += "i:"+str(rowindex)+";a:"+str(2)+":{i:0"+";s:"+str(len(row[0].encode('utf8')))+":\""+row[0]+"\";i:1;s:"
+
+            for colLang in range(1,numcol):
+                resultList[colLang-1] += strlistTranslation
+                #2:\"je\";}
+                resultList[colLang-1] += str(len(row[colLang].encode('utf8')))+":\""+row[colLang]+"\";}"
+
+            
+        for colLang in range(1,numcol):
+            #a:3:{i:0;a:2:{i:0;s:2:\"je\";i:1;s:2:\"je\"};i:1;a:2:{i:0;s:1:\"n\";i:1;s:4:\"nous\"};i:2;a:2:{i:0;s:1:\"v\";i:1;s:4:\"vous\"}}
+            resultList[colLang-1] = "a:"+ str(compteurligne) +":{"+resultList[colLang-1]+"}"
+
     
-    return result
+    return resultList
 
 
+def mergeWPJson(str1,str2):
+    pass
+     
 
+wpJson = csvToWpjson("traductionvirgu.csv")
 
-
-print(csvToWpjson("exemple.csv"))
-
-
-
-
-#fr,en
-#je,I
-
-
-#a:1:{i:0;a:2:{i:0;s:2:"je";i:1;s:1:"I"}}
-
-#a:1:{i:1;a:2:{i:0;s:2:'je';i:1;s:1:'I'}}x
+for st in wpJson:
+     print(st)
+     print("----------------------")
